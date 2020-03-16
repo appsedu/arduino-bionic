17e39e53b8e3eb35412e110b60b7ac8e62c0b4f0
ailtonbsj <ailton.ifce@gmail.com>
2020-03-16 11:56:46 -0300

    fixed bug java on github max size

--- compile.sh
+++ compile.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
 if [ ! -d deb_package/opt/arduino ]; then
-	wget "https://downloads.arduino.cc/arduino-1.8.9-linux64.tar.xz" -O arduino.tar.xz
+	wget "https://downloads.arduino.cc/arduino-1.8.12-linux64.tar.xz" -O arduino.tar.xz
 	tar -xJf arduino.tar.xz
 	mkdir -p deb_package/opt/
-	mv arduino-1.8.9 deb_package/opt/arduino
+	mv arduino-1.8.12 deb_package/opt/arduino
+	rm -rf deb_package/opt/arduino/java
 fi
 
 dpkg-deb -b deb_package .
